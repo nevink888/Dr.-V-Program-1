981d15460421c82fde4617ba1468d9037a29e070
nevink888 <113527599+nevink888@users.noreply.github.com>
2022-09-19 09:36:22 -0400

    Initial Commit

---
+++ Dr. V: Program 1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Dr. V: Program 1
+//
+//  Created by Nevin K on 9/19/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
