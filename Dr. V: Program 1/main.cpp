0e73516e5773ef5ca3c38bd85ca8359a96fdc628
nevink888 <113527599+nevink888@users.noreply.github.com>
2022-09-26 08:38:13 -0400

    Update main.cpp

--- Dr. V: Program 1/main.cpp
+++ Dr. V: Program 1/main.cpp
@@ -1,14 +1,31 @@
 //
 //  main.cpp
-//  Dr. V: Program 1
+//  Dr. V: Program 1: Functions on populations
 //
 //  Created by Nevin K on 9/19/22.
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    char deathrate = 'D';
+    char BegPopulation = 'P';
+    char birthrate = 'B';
+    
+    cout << "The formula for population growth/decline is displayed as the following: ";
+    cout << " P + ( B * P ) / 100 – ( D * P ) / 100/n" << endl; 
+    
+    
+    
+    
+    cout << " Enter the deathrate, beginnning population and the birth rate all as non-neg values " << endl;
+    cin >> deathrate;
+    cin >> BegPopulation;
+    cin >> birthrate;
+    
+    cout << deathrate + (birthrate * BegPopulation)/ 100 - (deathrate * BegPopulation)/ 100 << endl;
+    
     return 0;
 }
+
